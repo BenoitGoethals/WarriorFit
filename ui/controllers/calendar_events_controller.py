fcb7e6031364e1af8a733e557d1b7f4d18b0ce62
benoit <benoit.goethals@gmail.com>
2025-10-29 12:40:16 +0100

    Update `CalendarEventsController` to adjust event duration dynamically based on test type
    - Introduce `TypeFitnessTest` for determining session duration.
    - Modify session end time calculations to vary by test type (e.g., PHEF, FUNCTIONAL, COMBAT).
    - Refactor date handling for both sessions and crosses for consistency.

--- ui/controllers/calendar_events_controller.py
+++ ui/controllers/calendar_events_controller.py
@@ -1,5 +1,6 @@
 import datetime
 
+from core.type_fitness_test import TypeFitnessTest
 from services.service_cross import ServiceCross
 from services.service_test import ServiceTest
 
@@ -15,8 +16,14 @@ class CalendarEventsController:
         sessions=await self._service_test.get_all_test_sessions()
         
         for session in sessions:
-            session_date=session.datetime_start.date()
-            session_date_end = session_date + datetime.timedelta(hours=2)
+            session_date=session.datetime_start
+            if session.type_test == TypeFitnessTest.PHEF or session.type_test == TypeFitnessTest.FUNCTIONAL:
+                x=3
+            elif session.type_test == TypeFitnessTest.COMBAT:
+                x=5
+            else:
+                x=1
+            session_date_end = session_date + datetime.timedelta(hours=x)
             # {"id": "id1_allday", "title": "Event 1", "start": "2023-07-03", "end": "2023-07-05"},
             events_to_post.append({"id": session.id, "title": session.type_test.name,
                                    "start": session.datetime_start.strftime("%Y-%m-%dT%H:%M:%S"),
@@ -24,7 +31,7 @@ class CalendarEventsController:
         
         crosses=await self._service_cross.get_all_crosses()
         for cross in crosses:
-            cross_date=cross.datetime_start.date()
+            cross_date=cross.datetime_start
             cross_date_end=cross_date+ datetime.timedelta(hours=2)
             events_to_post.append({"id": cross.id, "title": "Cross", "start": cross_date.strftime("%Y-%m-%dT%H:%M:%S"), "end": cross_date_end.strftime("%Y-%m-%dT%H:%M:%S")})
         return events_to_post
