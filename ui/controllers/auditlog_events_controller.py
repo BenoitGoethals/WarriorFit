883b40def38a251c94bc403e06d163c71fa526aa
benoit <benoit.goethals@gmail.com>
2025-10-29 17:52:10 +0100

    Add refresh functionality to Audit Logs and optimize user retrieval:
    - Introduce "Refresh" button in UI for dynamic data updates.
    - Implement reactive mechanisms to trigger refresh and update the data frame.
    - Optimize audit log user retrieval by fetching all users at once.

--- ui/controllers/auditlog_events_controller.py
+++ ui/controllers/auditlog_events_controller.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Any
 import pandas as pd
 
+from data.db.db_model import User
 from services.service_user import UserService
 
 
@@ -10,22 +11,23 @@ class AuditLogEventsController:
 
     async def list_audit_logs_df(self) -> pd.DataFrame:
         logs: List[Any] = await self._service.get_audit_logs()
+        users:List[User] = await self._service.get_all_users()
+        
         if not logs:
-            return pd.DataFrame(columns=["ID", "User", "Action", "Details", "IP", "Created"])
+            return pd.DataFrame(columns=["User", "Action", "Details", "IP", "Created"])
         rows: List[Dict[str, Any]] = []
         for l in logs:
-            user = await self._service.get_user_by_id(getattr(l, "user_id", None))
+            user = next((user for user in users if user.id == getattr(l, "user_id", None)), None)
             rows.append(
                 {
-                    "ID": getattr(l, "id", None),
-                    "User": user.username if user else "",
+                    "User": user.username,
                     "Action": getattr(l, "action", ""),
                     "Details": getattr(l, "details", ""),
                     "IP": getattr(l, "ip_address", ""),
                     "Created": getattr(l, "created_at", ""),
                 }
             )
-
+       
         return pd.DataFrame(rows)
 
 
