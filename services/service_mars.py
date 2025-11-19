e2b97331c5fab33321498056e2afbd6ccb61f955
benoit <benoit.goethals@gmail.com>
2025-11-19 12:19:20 +0100

    Add Mars Tests UI and server-side logic:
    - Introduce `MarsPage` with UI for managing Mars tests, including forms, reactive controls, and data grid display.
    - Add server-side methods (`add_mars`, `update_mars`, `delete_mars`, `get_all_mars`) to handle CRUD operations.
    - Integrate `Mars` UI into navigation and app server via `app.py`.
    - Create `MarsController` to connect UI with `ServiceMars`.
    - Update `ServiceMars` methods to support async operations for database interaction.

--- services/service_mars.py
+++ services/service_mars.py
@@ -10,17 +10,17 @@ class ServiceMars(Service):
         self.be_mil_service = MilitaryService()
 
 
-    def get_all_mars(self):
+    async def get_all_mars(self):
         return self.__repo.get_all_mars()
 
-    def get_mars_by_id(self, ind_id):
+    async def get_mars_by_id(self, ind_id):
         return self.__repo.get_mars_by_id(ind_id)
 
-    def add_mars(self, mars):
+    async def add_mars(self, mars):
         ...
 
-    def delete_mars(self, ind_mars):
+    async def delete_mars(self, ind_mars):
         ...
 
-    def update_mars(self,id_mars):
+    async def update_mars(self,id_mars):
         ...
