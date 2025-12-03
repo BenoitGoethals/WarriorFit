425ef98f55d03457fb6d0c698c142c4a31415e35
benoit <benoit.goethals@gmail.com>
2025-12-03 20:59:45 +0100

    Fix typo in `deploy.sh` script (`udo` to `sudo`)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 sudo docker stop warriorfit-app
-udo docker rm warriorfit-ap
+sudo docker rm warriorfit-ap
 sudo docker run -d --restart unless-stopped --name warriorfit-app -p 8500:8000 warriorfit-app
 
 
