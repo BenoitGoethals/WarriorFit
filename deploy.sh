ce76697be1b593d0fb933613db001ff4515459bc
benoit <benoit.goethals@gmail.com>
2025-12-03 21:01:21 +0100

    Update `deploy.sh` to include Docker image build step before running the container

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 sudo docker stop warriorfit-app
 sudo docker rm warriorfit-ap
+sudo docker build -t warriorfit-app .
 sudo docker run -d --restart unless-stopped --name warriorfit-app -p 8500:8000 warriorfit-app
-
-
-
-
