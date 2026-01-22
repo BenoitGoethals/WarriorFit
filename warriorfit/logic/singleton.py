cd491d5c65bc93fbaadba86fd7748eb7668c7da6
benoit <benoit.goethals@gmail.com>
2026-01-22 20:41:11 +0100

    Remove `data_source_mapping.xml` and unused `ThreadSafeSingleton` implementation.

--- warriorfit/logic/singleton.py
+++ warriorfit/logic/singleton.py
@@ -14,30 +14,3 @@ class Singleton(ABCMeta):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
-
-
-class ThreadSafeSingleton(metaclass=ABCMeta):
-    """
-       Metaclass for creating thread-safe Singleton classes.
-       Any class using this metaclass will automatically be a singleton.
-       """
-    # Dictionary to store instances of different classes
-    _instances = {}
-
-    # Lock to ensure thread-safe singleton instantiation
-    _lock = threading.Lock()
-
-    def __call__(cls, *args, **kwargs):
-        """
-        Override __call__ to control instance creation.
-        This method is called when a class is instantiated.
-        """
-        # Acquire the lock to ensure thread safety
-        with cls._lock:
-            # Check if an instance of this class already exists
-            if cls not in cls._instances:
-                # Create a new instance and store it in the dictionary
-                cls._instances[cls] = super().__call__(*args, **kwargs)
-            # Return the singleton instance
-            return cls._instances[cls]
-
