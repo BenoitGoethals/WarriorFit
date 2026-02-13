7e1a13776365aac00d24c39f03fea95e74788b15
benoit <benoit.goethals@gmail.com>
2026-02-13 16:49:44 +0100

    fix Gender literal

--- warriorfit/core/Gender.py
+++ warriorfit/core/Gender.py
@@ -5,6 +5,15 @@ class Gender(Enum):
     M = "M"
     F = "F"
 
+
+    @classmethod
+    def literals(cls) -> list[str]:
+        return [member.value for member in cls]
+
+    def to_literal(self):
+        return self.name
+
+
     def __str__(self):
         return self.value
 
